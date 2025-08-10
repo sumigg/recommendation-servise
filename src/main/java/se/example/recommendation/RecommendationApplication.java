2bd8c2ee28dd78b58f1a9d29368af812fbddee22
sumigg <with.sumitaggarwal@gmail.com>
2025-08-10 12:21:39 +0530

    add initial project for recommendation

---
+++ src/main/java/se/example/recommendation/RecommendationApplication.java
@@ -0,0 +1,19 @@
+package se.example.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {
+		"se.example.recommendation",
+		"se.example.api.core.recommendation",
+		"se.example.util.http"
+})
+public class RecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationApplication.class, args);
+	}
+
+}
